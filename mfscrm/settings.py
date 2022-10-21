9045ff35529c09c4f2aec1f2ea3a11293349656b
rishikakikkuri <43698970+rishikakikkuri@users.noreply.github.com>
2022-10-21 18:41:40 -0500

    debug

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-z6u66kxkz3-8baew!jb4-)s16s@75fgyj&m!fgdxij1b4!e(6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
